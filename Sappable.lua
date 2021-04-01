c8afa456a0e1c03ce6d77e4e670a1febae5320c5
kbabs17 <53099857+kbabs17@users.noreply.github.com>
2021-04-01 14:29:50 -0400

    Update Sappable.lua
    Added locale check to update sappable creature types for different languages

--- Sappable.lua
+++ Sappable.lua
@@ -1,12 +1,54 @@
-local function defaultFunc(L, key)
-	return key;
+--local function defaultFunc(L, key)
+--	return key;
+--end
+
+--MyLocalizationTable = setmetatable({}, {__index=defaultFunc});
+
+--local L = MyLocalizationTable;
+
+--sappableCreatureTypes = {L["Humanoid"], L["Beast"], L["Demon"], L["Dragonkin"]};
+
+local LOCALE = GetLocale()
+
+if LOCALE == "enUS" then
+sappableCreatureTypes = {"Humanoid", "Beast", "Demon", "Dragonkin"};
 end
 
-MyLocalizationTable = setmetatable({}, {__index=defaultFunc});
+if LOCALE == "deDE" then
+	sappableCreatureTypes = {"Humanoid", "Wildtier", "Dämon", "Drachkin"};
+end
 
-local L = MyLocalizationTable;
+if LOCALE == "esES" then
+	sappableCreatureTypes = {"Humanoide", "Bestia", "Demonio", "Dragonante"};
+end
 
-sappableCreatureTypes = {L["Humanoid"], L["Beast"], L["Demon"], L["Dragonkin"]};
+if LOCALE == "esMX" then
+	sappableCreatureTypes = {"Humanoide", "Bestia", "Demonio", "Dragonante"};
+end
+
+if LOCALE == "frFR" then
+	sappableCreatureTypes = {"d’humanoïde", "de bête", "Démon", "draconiques"};
+end
+
+if LOCALE == "itIT" then
+	sappableCreatureTypes = {"Umanoidi", "Bestiali", "Demone", "Dragoidi"};
+end
+
+if LOCALE == "koKR" then
+	sappableCreatureTypes = {"인간형", "야수", "악마", "용족"};
+end
+
+if LOCALE == "ptBR" then
+	sappableCreatureTypes = {"Humanoide", "Fera", "Demônio", "Draconiano"};
+end
+
+if LOCALE == "zhCN" then
+	sappableCreatureTypes = {"人型", "野兽", "恶魔", "龙类"};
+end
+
+if LOCALE == "zhTW" then
+	sappableCreatureTypes = {"人形", "野獸", "惡魔", "龍類"};
+end
 
 targetIndicator=CreateFrame("Frame");
 targetIndicator:SetParent(TargetFrame);
